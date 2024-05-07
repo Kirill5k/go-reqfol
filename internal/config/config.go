ada074cb1baab6dfcda8dca314137e79bac49e5b
kirill5k <immotional@aol.com>
2024-05-07 10:43:22 +0100

    Tidy up config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,13 +1,42 @@
 package config
 
-import "time"
+import (
+	"github.com/spf13/viper"
+	"log"
+	"time"
+)
+
+type Server struct {
+	Port int `mapstructure:"port"`
+}
 
 type Client struct {
-	MaxIdleConns        int
-	MaxIdleConnsPerHost int
-	IdleConnTimeout     time.Duration
-	Timeout             time.Duration
-	RetryCount          int
-	RetryWaitTime       time.Duration
-	RetryMaxWaitTime    time.Duration
+	MaxIdleConns        int           `mapstructure:"max-idle-conns"`
+	MaxIdleConnsPerHost int           `mapstructure:"max-idle-conns-per-host"`
+	IdleConnTimeout     time.Duration `mapstructure:"idle-conn-timeout"`
+	Timeout             time.Duration `mapstructure:"timeout"`
+	RetryCount          int           `mapstructure:"retry-count"`
+	RetryWaitTime       time.Duration `mapstructure:"retry-wait-time"`
+	RetryMaxWaitTime    time.Duration `mapstructure:"retry-max-wait-time"`
+}
+
+type App struct {
+	Server Server
+	Client Client
+}
+
+func LoadAppConfig() *App {
+	v := viper.New()
+	v.SetConfigName("application")
+	v.SetConfigType("yaml")
+	v.AddConfigPath("./internal/config")
+	if err := v.ReadInConfig(); err != nil {
+		log.Fatalf("failed to read viper config. %v", err)
+	}
+
+	var app App
+	if err := v.Unmarshal(&app); err != nil {
+		log.Fatalf("failed to decode viper config into struct. %v", err)
+	}
+	return &app
 }
